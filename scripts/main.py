32be1d964bcf9f1ba809b8d12780783fcef862ec
Thomas Eby <tgeby@icloud.com>
2026-02-04 13:46:38 -0500

    DFs now only load necessary columns with 'usecols' argument

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,25 @@
 from data_processing.df_load_driver import get_data_frames
 
 def main():
-    # Load data
-    df = get_data_frames()
+    # Load data and standardize
+    [national_df, new_york_df, oregon_df, california_df] = get_data_frames()
+
+    # Break data into tables
 
     # Write data to db
-    print(df)
+    print('-----------------------')
+    print("National DF")
+    print(national_df.head())
+    print('-----------------------')
+    print("New York DF")
+    print(new_york_df.head())
+    print('-----------------------')
+    print("Oregon DF")
+    print(oregon_df.head())
+    print('-----------------------')
+    print("California DF")
+    print(california_df.head())
+    print('-----------------------')
 
 if __name__ == "__main__":
     main()
