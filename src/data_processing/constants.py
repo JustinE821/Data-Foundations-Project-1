1cf45d82f84a55c2476a095d67be98d4f1974b61
Thomas Eby <tgeby@icloud.com>
2026-02-04 13:56:19 -0500

    Refactor: pulled constants into data_processing.constants module

--- src/data_processing/constants.py
+++ src/data_processing/constants.py
@@ -1,3 +1,31 @@
+from pathlib import Path
+
+# Paths to datasets
+SCRIPT_DIR_PATH = Path(__file__).parent.resolve()
+CSV_FILES_DIR_RELATIVE_PATH = SCRIPT_DIR_PATH / '..' / '..' / 'data'
+NATIONAL_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'National_Interagency_Fire_Occurrence_Sixth_Edition_1992-2020_(Feature_Layer).csv'
+NEW_YORK_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'New_York_State_Forest_Ranger_Wildland_Fire_Reporting_Database__Beginning_2008.csv'
+OREGON_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'ODF_Fire_Occurrence_Data_2000-2022.csv'
+CALIFORNIA_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'California_Historic_Fire_Perimeters_-6236829869961296710.csv'
+
+# Names of relevant columns in raw datasets
+NATIONAL_COLS = ['LONGITUDE', 'LATITUDE', 'STATE', 'FIRE_NAME', 'DISCOVERY_DATE', 'CONT_DATE', 'NWCG_GENERAL_CAUSE', 'FIRE_SIZE', 'FIRE_SIZE_CLASS']
+# Note: New York dataset does not have columns for state or size_class
+# We will have to insert a state column with 'NY' for its values
+# We will have to calculate the size class for each record based on its acreage 
+NEW_YORK_COLS = ['Longitude', 'Latitude', 'Incident Name', 'Fire Start Date', 'Initial Report Date', 'Fire Out Date', 'Cause', 'Acreage'] 
+# Note: Oregon dataset does not have a state column
+# We will have to insert a state column with 'OR' for its values
+OREGON_COLS = ['Long_DD', 'Lat_DD', 'FireName', 'ReportDateTime', 'Control_DateTime', 'GeneralCause', 'EstTotalAcres', 'Size_class']
+# Note: California dataset does not have longitude, latitude, and size_class columns
+# We will have to insert longitude and latitude columns with null values
+# We will have to calculate the size class for each record based on its acreage
+CALIFONIA_COLS = ['State', 'Fire Name', 'Alarm Date', 'Containment Date', 'Cause', 'GIS Calculated Acres']
+
+# Standard column names that we will convert DataFrames into
+STANDARD_COLUMN_NAMES = ['longitude', 'latitude', 'state', 'fire_name', 'report_date', 'containment_date', 'cause', 'acreage', 'size_class']
+
+# Names of columns in our output data
 WILDFIRE_COLUMN_NAMES = ['wildfire_id', 'state', 'fire_name', 'cause_id']
 WILDFIRE_SIZE_COLUMN_NAMES = ['wildfire_id', 'size_class', 'acreage']
 WILDFIRE_SIZE_CLASS_COLUMN_NAMES = ['size_class', 'min_acreage', 'max_acreage']
--- src/data_processing/df_load_driver.py
+++ src/data_processing/df_load_driver.py
@@ -1,31 +1,7 @@
 import pandas as pd
-from pathlib import Path
-
-# Paths to datasets
-SCRIPT_DIR_PATH = Path(__file__).parent.resolve()
-CSV_FILES_DIR_RELATIVE_PATH = SCRIPT_DIR_PATH / '..' / '..' / 'data'
-NATIONAL_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'National_Interagency_Fire_Occurrence_Sixth_Edition_1992-2020_(Feature_Layer).csv'
-NEW_YORK_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'New_York_State_Forest_Ranger_Wildland_Fire_Reporting_Database__Beginning_2008.csv'
-OREGON_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'ODF_Fire_Occurrence_Data_2000-2022.csv'
-CALIFORNIA_PATH = CSV_FILES_DIR_RELATIVE_PATH / 'California_Historic_Fire_Perimeters_-6236829869961296710.csv'
-
-# Standard column names that we will convert DataFrames into
-STANDARD_COLUMN_NAMES = ['longitude', 'latitude', 'state', 'fire_name', 'report_date', 'containment_date', 'cause', 'acreage', 'size_class']
-
-# Columns to load per dataset
-NATIONAL_COLS = ['LONGITUDE', 'LATITUDE', 'STATE', 'FIRE_NAME', 'DISCOVERY_DATE', 'CONT_DATE', 'NWCG_GENERAL_CAUSE', 'FIRE_SIZE', 'FIRE_SIZE_CLASS']
-# Note: New York dataset does not have columns for state or size_class
-# We will have to insert a state column with 'NY' for its values
-# We will have to calculate the size class for each record based on its acreage 
-NEW_YORK_COLS = ['Longitude', 'Latitude', 'Incident Name', 'Fire Start Date', 'Initial Report Date', 'Fire Out Date', 'Cause', 'Acreage'] 
-# Note: Oregon dataset does not have a state column
-# We will have to insert a state column with 'OR' for its values
-OREGON_COLS = ['Long_DD', 'Lat_DD', 'FireName', 'ReportDateTime', 'Control_DateTime', 'GeneralCause', 'EstTotalAcres', 'Size_class']
-# Note: California dataset does not have longitude, latitude, and size_class columns
-# We will have to insert longitude and latitude columns with null values
-# We will have to calculate the size class for each record based on its acreage
-CALIFONIA_COLS = ['State', 'Fire Name', 'Alarm Date', 'Containment Date', 'Cause', 'GIS Calculated Acres']
-
+from data_processing.constants import NATIONAL_PATH, NEW_YORK_PATH, OREGON_PATH, CALIFORNIA_PATH
+from data_processing.constants import NATIONAL_COLS, NEW_YORK_COLS, OREGON_COLS, CALIFONIA_COLS
+from data_processing.constants import STANDARD_COLUMN_NAMES
 
 def get_data_frames():
     national_df = pd.read_csv(NATIONAL_PATH, usecols=NATIONAL_COLS, nrows=5)
