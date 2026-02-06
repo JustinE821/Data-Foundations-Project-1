ce2deacaf804948c2472c0249f35c6f842a1db30
JustinE821 <justinevans124@outlook.com>
2026-02-06 15:35:46 -0500

    Renamed df_upload to db_upload. Also updated requirements.txt

---
+++ src/data_upload/db_upload.py
@@ -0,0 +1,14 @@
+'''
+Docstring for data_upload.df_upload
+This file will contain the functions used to add data to the database as well as any other queries
+'''
+import psycopg2
+from psycopg2 import sql
+
+from data_upload.db_connection import init_conn
+
+def create_wildfire_entries(df):
+     return 0
+
+
+
--- src/data_upload/df_upload.py
+++
@@ -1,8 +0,0 @@
-'''
-Docstring for data_upload.df_upload
-This file will contain the functions used to add data to the database
-'''
-
-
-
-
